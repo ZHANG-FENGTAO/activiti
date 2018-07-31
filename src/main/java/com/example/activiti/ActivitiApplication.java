584fd4302c8b9531ca964147bf96190c1db67889
zhangfengtao <zhangfu0072009@163.com>
2018-08-01 04:03:28 +0800

    11

--- src/main/java/com/example/activiti/ActivitiApplication.java
+++ src/main/java/com/example/activiti/ActivitiApplication.java
@@ -1,9 +1,13 @@
 package com.example.activiti;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+		org.activiti.spring.boot.SecurityAutoConfiguration.class
+})
 public class ActivitiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/activiti/helloword/HelloWorld.java
+++ src/main/java/com/example/activiti/helloword/HelloWorld.java
@@ -1,12 +1,23 @@
 package com.example.activiti.helloword;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author zft
  * @date 2018/6/5.
  */
+@RestController
+@RequestMapping("/api/test")
 public class HelloWorld {
 
+    @RequestMapping("/hi")
+    public String test(){
 
+        return "hello world";
+    }
 
 
 }
